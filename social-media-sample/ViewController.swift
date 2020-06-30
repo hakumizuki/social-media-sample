4d4df7b84e889360fe14eb2b26bec12a38c61507
Taichi Masuyama <montanha.masu536@gmail.com>
2020-06-30 20:24:02 +0900

    Initial Commit

---
+++ social-media-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  social-media-sample
+//
+//  Created by 増山太一 on 2020/06/30.
+//  Copyright © 2020 Haku Mizuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
